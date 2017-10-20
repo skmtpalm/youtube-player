1b2505ff189d7cc30cb9d72eb9d57e16e8c5fcae
skmtpalm <hachihachi.8og@gmail.com>
2017-10-20 23:41:22 +0900

    Update Video Details

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,6 +1,34 @@
-<template lang="html">
+<template>
   <aside>
-    <p>Video list</p>
+    <article class="media">
+      <figure class="media-left">
+        <p class="image is-128x72">
+          <img src="http://placehold.it/480x270" />
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <a class="#">
+              <strong>Thumbnails title</strong>
+            </a><br />
+            <span class="is-size-7 has-text-grey">Traversary Media</span>
+          </p>
+        </div>
+      </div>
+    </article>
+    <article class="media">
+      <figure class="media-left">
+        <p class="image is-128x128">
+          <img src="http://placehold.it/480x270" />
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <p><strong>Thumbnails title</strong></p>
+        </div>
+      </div>
+    </article>
   </aside>
 </template>
 
@@ -10,4 +38,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.is-128x72 {
+  width: 128px;
+  height: 72px;
+}
 </style>
