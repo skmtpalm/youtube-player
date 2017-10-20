04f7e2afde4a3fed4e2a277f1b9ccad974476bf6
skmtpalm <hachihachi.8og@gmail.com>
2017-10-20 16:28:30 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <section class="hero is-primary">
+      <section class="hero-body">
+        <h1>{{ title }}</h1>
+      </section>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'You tube player using Vue and Bulma'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../node_modules/bulma/bulma.sass";
+</style>
